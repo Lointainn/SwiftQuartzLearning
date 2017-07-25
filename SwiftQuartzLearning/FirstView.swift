ca88a17166d5398d8901571f9a965d65bbfaec3f
吴宗帅 <wuzongshuai@126.com>
2017-07-25 23:14:35 +0800

    整理下第一个

--- SwiftQuartzLearning/FirstView.swift
+++ SwiftQuartzLearning/FirstView.swift
@@ -16,12 +16,22 @@ class FirstView: UIView {
     
     override func draw(_ rect: CGRect) {
         let context = UIGraphicsGetCurrentContext()!
+        drawFirstLine(context: context)
+        
+        drawSecondLines(context: context)
+       
+        drawThirdLines(context: context)
+    }
+    
+    func drawFirstLine(context: CGContext) {
         context.setStrokeColor(red: 0, green: 0, blue: 1, alpha: 1)
         context.setLineWidth(2)
         context.move(to: CGPoint(x: 10, y: 100))
         context.addLine(to: CGPoint(x: 300, y: 100))
         context.strokePath()
-        
+    }
+    
+    func drawSecondLines(context: CGContext) {
         let points = [
             CGPoint(x: 100, y: 200),
             CGPoint(x: 150, y: 150),
@@ -29,21 +39,22 @@ class FirstView: UIView {
             CGPoint(x: 250, y: 150),
             CGPoint(x: 300, y: 200),
             CGPoint(x: 350, y: 150),
-        ]
+            ]
         
         context.addLines(between: points)
         context.strokePath()
-        
+    }
+    
+    func drawThirdLines(context: CGContext) {
         let points2 = [
-            CGPoint(x: 100, y: 400),
-            CGPoint(x: 150, y: 350),
-            CGPoint(x: 200, y: 400),
-            CGPoint(x: 250, y: 350),
-            CGPoint(x: 300, y: 400),
-            CGPoint(x: 350, y: 350),
-        ]
+            CGPoint(x: 100, y: 300),
+            CGPoint(x: 150, y: 250),
+            CGPoint(x: 200, y: 300),
+            CGPoint(x: 250, y: 250),
+            CGPoint(x: 300, y: 300),
+            CGPoint(x: 350, y: 250),
+            ]
         context.strokeLineSegments(between: points2)
-
     }
     
 
