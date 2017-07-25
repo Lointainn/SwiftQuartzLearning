01ff2f47fd67b6d8c1968ca361bc57da75a48de3
吴宗帅 <wuzongshuai@126.com>
2017-07-25 21:22:25 +0800

    Initial Commit

---
+++ SwiftQuartzLearningTests/SwiftQuartzLearningTests.swift
@@ -0,0 +1,36 @@
+//
+//  SwiftQuartzLearningTests.swift
+//  SwiftQuartzLearningTests
+//
+//  Created by Tusky on 2017/7/25.
+//  Copyright © 2017年 Tusky. All rights reserved.
+//
+
+import XCTest
+@testable import SwiftQuartzLearning
+
+class SwiftQuartzLearningTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
